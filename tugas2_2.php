bc2b68f1fd8ab66a44276d13ce40626fe2620820
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:03:41 +0700

    Add files via upload

---
+++ tugas2_2.php
@@ -0,0 +1,9 @@
+<?php 
+    $jual = 80000;
+    $beli = 50000;
+    $keuntungan = $jual - $beli;
+
+    echo "<h5>Jual = $jual </h5>";
+    echo "<h5>Beli = $beli </h5>";
+    echo "<h5>Keuntungan = $keuntungan </h5>";
+ ?>
